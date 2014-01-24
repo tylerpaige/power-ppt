e5842a85bfedc722f7bfb646bead828a64b0a338
Tyler Paige <tylerclarkpaige@gmail.com>
2014-01-24 19:20:55 +0100

    Now with more options for functionality and styling

--- README.md
+++ README.md
@@ -33,7 +33,10 @@ Using this plugin (ironically) relies on a pretty strict structure. Please use `
 </div>
 
 <div id="s2" class="slide">
-    <div class="content">Content for slide #s2</div>
+    <div class="content">
+        <p>Content for slide #s2</p>
+        <div class="caption">A basic caption for this slide</div>
+    </div>
 </div>
 
 <div id="s3" class="slide">
@@ -51,6 +54,13 @@ Using this plugin (ironically) relies on a pretty strict structure. Please use `
         <div class="content">Content for the linear progression of the tangent off slide #s3</div>
     </div>
 </div>
+
+<div id="s4" class="slide">
+    <div class="content">
+        <h1>Centered text for slide #s4</h1>
+        <img src="images/bg.jpg"/> <!-- This image will be made the slide's background, unless the bgimgs option is set to false -->
+    </div>
+</div>
 ```
 
 
@@ -59,3 +69,48 @@ Initialize the plugin by selecting the slideshow container and passing a selecto
 ``` js
 $('#container').powerPPT('.slide');
 ```
+
+## Options
+- `option` : description
+	- **defaultValue**
+	- _possible value_
+	
+- `bgimgs` : if an `<img>` element is found in the slide's content, make that image the slide's background image
+    - **true**
+    - *false*
+- `controls` : display a 4-arrow indicator depending on what directions a user can navigate from the current slide
+    - **false**
+    - **true** (not totally smooth yet)
+- `loader` : loader image for slides that are still downloading images
+    - **false**
+    - _path/to/image.gif_
+- `origin` : manually choose which slide is the "starting" slide if it should be something other than the first in the DOM
+    - **false** (i.e. the first slide in the DOM)
+    - _jQuery selector_ (e.g. `#start`)
+- `slide` : jQuery selector for all slides in the slideshow
+    - **.slide**
+    - _jQuery selector_ (e.g. `.box`)
+    
+## Style Options
+I've included a few styles you can use for your slides. [Check out the demo](http://tylerpaige.com/secrets/powerppt/example/) to see a demonstration of each style.
+
+- Background image styles
+    - Unless you've set the `bgimgs` option to false, powerPPT will make an `img` element in the `.content` the slide's background image. 
+    - These classes will control the image's fitting when applied to the `.content`
+    - default: image centered in the middle of the frame
+    - `.fill` or `.cover`: enlarge the image until it covers the entire frame
+    - `.stretch` or `.contain`: enlarge the image until one dimension of the image matches the window's dimension
+- Text styles: classes that change the appearance of text when applied to the `.content` element
+    - `.white`: white text
+    - `.yellow`: yellow text
+    - `.outline`: give 1px outline to text (white outline for black text, black outline for white/yellow text)
+    - `.yellowOutline`: yellow outline
+    - `.shadow`: add a subtle dropshadow to the text
+- Text position formatting
+    - Default: horizontally and vertically centered, black
+    - `.text`: allows the text to scroll vertically. *Apply the entire slide, not just the content*
+- Captions: classes for an element `.caption` **in** the `.content` element
+    - Default: aligned to bottom of frame, centered, black. All text styles can be applied specifically to a caption.
+    - `.gradient`: applies a black-to-transparent background gradient to the caption
+    - `.whiteGradient`: same as above, but with a white gradient
+    - `.block`: applies a solid white background the caption
--- powerppt.js
+++ powerppt.js
@@ -1,55 +1,123 @@
 (function($){ 
-   $.fn.powerPPT = function(slide) {
-       if(!slide){ slide='.slide'; }
-           
-       var transitionEnd = 'transitionend webkitTransitionEnd oTransitionEnd otransitionend animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd',
-           index,
-           cur = $(this).children(slide).eq(0),
-           slideClass = slide.substr(1);
-       
-           
-       $(cur).addClass('visible');
-       
-        keypress.combo('right', function() {
-            index = $(cur).siblings(slide).addBack().index(cur);
-            if(index < $(cur).siblings(slide).length){
-                target = index+1; 
-                cur = $(cur).siblings(slide).addBack().eq(target);    		
-                $(cur).addClass('slideLeft recent').one(transitionEnd, function(){ 
-                    $(this).addClass('visible').removeClass('slideLeft recent').prevAll().removeClass('visible slideLeft slideRight recent');
-                }); 
-                $('.recent').not(cur).removeClass('recent');
+    $.fn.extend({
+        powerPPT: function(options) {
+            var defaults = {
+                bgimgs: true,
+                controls: false,
+                loader: false,
+                origin: false,
+                slide: '.slide'
             }
-        });
-        keypress.combo('left', function() {
-            index = $(cur).siblings(slide).addBack().index(cur);
-            if(index > 0){
-                target = index-1;
-                cur = $(cur).siblings(slide).addBack().eq(target);
+            
+            var options = $.extend(defaults, options);
+            
+            return this.each(function() {
+                var o = options,
+                    transitionEnd = 'transitionend webkitTransitionEnd oTransitionEnd otransitionend animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd',
+                    index,
+                    slide = o.slide,
+                    slides = $(this).find(slide),
+                    slideClass = slide.substr(1);
+                if(o.loader != false){
+                    var loader = '<div class="loader"><img src="'+o.loader+'" /><br/>img loading</div>';
+                } else {
+                    var loader;
+                }
+                    
+                if(o.origin == false){ cur = $(this).find(slide).eq(0); }
+                else { cur = $(this).find(o.origin); }
+                $(cur).addClass('visible');
                 
-                $(cur).addClass('slideRight recent').one(transitionEnd, function() {
-                   $(this).addClass('visible').removeClass('slideRight recent').nextAll().removeClass('visible slideRight recent');
-                });
-                $('.recent').not(cur).removeClass('recent'); //Only most recent advance should be called "Recent"
-            }
-        });
-        keypress.combo('down', function() {
-            if($(cur).children(slide).length > 0){
-                cur = $(cur).children(slide).eq(0);
-                $(cur).addClass('slideUp').one(transitionEnd, function() {
-                    $(this).addClass('visible').removeClass('slideUp').parent('.visible').removeClass('visible');
-                });
-            }
-        });
-        keypress.combo('up', function() {
-            if($(cur).parent().hasClass(slideClass)){ // only ascend if the target is a slide, not the container
-                index = cur;
-                cur = cur.parent();
-                index.siblings('.content').parent().addBack().addClass('visible');
-                index.addClass('slideOut').one(transitionEnd, function() {
-                    $(this).removeClass('visible slideOut').siblings().removeClass('slideOut recent visible');
-                });
-            }
-        });
-   } 
+                if(o.controls==true){
+                    var controlsMarkup = '<div id="controls"><div class="up">↑</div><div class="left">←</div><div class="right">→</div><div class="down">↓</div></div>';
+                    $(this).prepend(controlsMarkup);
+                    var controls = $('#controls'),
+                        conUp = $(controls).children('.up'),
+                        conRight = $(controls).children('.right'),
+                        conDown = $(controls).children('.down'),
+                        conLeft = $(controls).children('.left'),
+                        manageControls = function(slide) {
+                            if(slide.parent().hasClass(slideClass) == false){ $(conUp).fadeOut(); }
+                            else if($(conUp).is(':hidden')) { $(conUp).fadeIn(); }
+                            
+                            if(slide.next().hasClass(slideClass) == false){ $(conRight).fadeOut(); }
+                            else if($(conRight).is(':hidden')) { $(conRight).fadeIn(); }
+                            
+                            if(slide.prev().hasClass(slideClass) == false){ $(conLeft).fadeOut(); }
+                            else if($(conLeft).is(':hidden')) { $(conLeft).fadeIn(); }
+                            
+                            if(slide.children('.'+slideClass).length <= 0){ $(conDown).fadeOut(); }
+                            else if($(conDown).is(':hidden')) { $(conDown).fadeIn(); }
+                        };
+                } else {
+                    var manageControls = function(){ return false; };
+                }
+                if(o.bgimgs == true){
+                    $.each(slides, function() {
+                         var slide = $(this).children('.content'),
+                             img = $(slide).children('img');
+                         if(img.length > 0){
+                             var url = $(img).attr('src');
+                             $(img).hide();
+                             $(slide).prepend(loader);
+                             $(img).one('load', function(){
+                                 $(slide).css("background-image", "url('"+url+"')").children('.loader').remove();
+                                 $(img).remove();
+                             });
+                         }
+                    });
+                }
+                
+                 keypress.combo('right', function() {
+                     index = $(cur).siblings(slide).addBack().index(cur);
+                     if(index < $(cur).siblings(slide).length){
+                         target = index+1; 
+                         cur = $(cur).siblings(slide).addBack().eq(target);    		
+                         $(cur).addClass('slideLeft recent').one(transitionEnd, function(){ 
+                             $(this).addClass('visible').removeClass('slideLeft recent').prevAll().removeClass('visible slideLeft slideRight recent');
+                         }); 
+                         $('.recent').not(cur).removeClass('recent');
+                     }
+                     manageControls($(cur));
+                 });
+                 keypress.combo('left', function() {
+                     index = $(cur).siblings(slide).addBack().index(cur);
+                     if(index > 0){
+                         target = index-1;
+                         cur = $(cur).siblings(slide).addBack().eq(target);
+                         
+                         $(cur).addClass('slideRight recent').one(transitionEnd, function() {
+                            $(this).addClass('visible').removeClass('slideRight recent').nextAll().removeClass('visible slideRight recent');
+                         });
+                         $('.recent').not(cur).removeClass('recent'); //Only most recent advance should be called "Recent"
+                     }
+                     manageControls($(cur));
+                 });
+                 keypress.combo('down', function() {
+                     if($(cur).children(slide).length > 0){
+                         cur = $(cur).children(slide).eq(0);
+                         $(cur).addClass('slideUp').one(transitionEnd, function() {
+                             $(this).addClass('visible').removeClass('slideUp').parent('.visible').removeClass('visible');
+                         });
+                     }
+                     manageControls($(cur));
+                 });
+                 keypress.combo('up', function() {
+                     if($(cur).parent().hasClass(slideClass)){ // only ascend if the target is a slide, not the container
+                         index = cur;
+                         cur = cur.parent();
+                         index.siblings('.content').parent().addBack().addClass('visible');
+                         index.addClass('slideOut').one(transitionEnd, function() {
+                             $(this).removeClass('visible slideOut').siblings().removeClass('slideOut recent visible');
+                         });
+                         manageControls(cur);
+                     }
+                    
+                 });
+                 
+                 manageControls($(cur));
+                 
+            });
+        }
+    });
 })(jQuery);
