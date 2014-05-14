60d802d9eb831346168928cb315d09ef03e8dd67
Tyler Paige <tylerclarkpaige@gmail.com>
2014-05-14 00:31:03 -0400

    Jump to slide, jump to root, new docs

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # PowerPPT
 
-## Live Demo
-*Check out a simple demonstration here: [http://tylerpaige.com/secrets/powerppt/example](http://tylerpaige.com/secrets/powerppt/example)*
-
 This is a pretty simple way to create a multi-directional "Powerpoint" slideshow. Its purpose to allow for tangents in format that is fairly linear. While a normal slide show progresses from Slide #1 to #2 to #3 in a linear progression, this jQuery plugin allows the user to go on a tangent off a slide.
 
 In this way, a user has two basic options at every slide: to progress linearly along the same trajectory (i.e. to advance to the "next" slide), or to go off on a tangent. If the user goes off on a tangent, they can then either follow that tangent or go off on another tangent.
@@ -19,6 +16,11 @@ The live slideshow is controlled using the arrow keys.
 ### Backtracking
 - **←** will take the slideshow to previous slide in the continuous linear progression. 
 - **↑** will take the slideshow to last point of departure. If a user is on a tangent, pressing **↑** will return to the origin of the tangent.
+- **page up** will take the slideshow to the initial point of departure from the initial track. This is an experimental feature that may behave unexpectedly in complex slideshow structures.
+
+### Jumping
+- **esc** will jump to slides that are out of order. See "Slide Jumping Syntax" for more information
+
 
 ## Usage
 *Usage isn't exactly the most friendly right now...*
@@ -36,7 +38,10 @@ Using this plugin (ironically) relies on a pretty strict structure. Please use `
 </div>
 
 <div id="s2" class="slide">
-    <div class="content">Content for slide #s2</div>
+    <div class="content">
+        <p>Content for slide #s2</p>
+        <div class="caption">A basic caption for this slide</div>
+    </div>
 </div>
 
 <div id="s3" class="slide">
@@ -54,6 +59,13 @@ Using this plugin (ironically) relies on a pretty strict structure. Please use `
         <div class="content">Content for the linear progression of the tangent off slide #s3</div>
     </div>
 </div>
+
+<div id="s4" class="slide">
+    <div class="content">
+        <h1>Centered text for slide #s4</h1>
+        <img src="images/bg.jpg"/> <!-- This image will be made the slide's background, unless the bgimgs option is set to false -->
+    </div>
+</div>
 ```
 
 
@@ -62,3 +74,73 @@ Initialize the plugin by selecting the slideshow container and passing a selecto
 ``` js
 $('#container').powerPPT('.slide');
 ```
+
+## Slide Jumping Syntax
+Currently working on a feature that allows users to jump from one slide to another, skipping over the logical next slides in the DOM. This is done simply with a data- attribute on the sending slide and a jQuery selector.
+
+The jump movement is mapped to the escape key, though this is subject to change in future versions.
+
+PowerPPT remembers the source of a jump, so hitting the escape key after jumping to a slide will return to the referrer. 
+
+``` html
+<div class="slide" data-target="#jumpHere">
+	<div class="content">Hitting the 'escape' key here will jump to the slide selected by the 'data-target' attribute.</div>
+</div>
+<div class="slide">
+	<div class="content">So this slide will be skipped.</div>
+</div>
+<div id="jumpHere" class="slide">
+	<div class="content">This is the target for the jump. Hitting escape AGAIN will bring you back</div>
+</div>
+```
+
+##
+
+## Options
+
+Option    | Description     | Default Value    | Possible Values
+-----------------------------------------------------------------
+bgimgs    | **boolean**: if an `<img>` element is found in the slide's content, make that image the slide's background image | true | false
+controls | **boolean**: display a 4-arrow indicator depending on what directions a user can navigate from the current slide | false | true
+loader | **URL**: loader image for slides that are still downloading images | false (no loader) | _path/to/image.gif_
+origin | **jQuery selector**: manually choose which slide is the "starting" slide if it should be something other than the first in the DOM | false | _jQuery selector_
+slide  | **jQuery selector**: selector for all slides in the slideshow | `.slide` | _jQuery selector_
+
+    
+## Style Options
+I've included a few styles you can use for your slides. [Check out the demo](http://tylerpaige.com/secrets/powerppt/example/) to see a demonstration of each style.
+
+#### Background image styles
+Unless you've set the `bgimgs` option to false, powerPPT will make an `img` element in the `.content` the slide's background image. These classes will control the image's fitting when applied to the `.content`
+Class | Description
+-------------------
+*default* | image centered in the middle of the frame | *N/A*
+`.fill` or `.cover` | enlarge the image until it covers the entire frame
+`.stretch` or `.contain` | enlarge the image until it covers the frame, but do not crop either dimension
+
+#### Text Styles
+Classes that change the appearance of text when applied to the `.content` element
+Class | Description
+-------------------
+`.white`| white text
+`.yellow`| yellow text
+`.outline`| give 1px outline to text (white outline for black text, black outline for white/yellow text)
+`.yellowOutline`| yellow outline
+`.shadow`| add a subtle dropshadow to the text
+
+#### Text Positioning
+Class | Description
+-------------------
+*default* | if it is the only child element of `.content`, horizontally and vertically centered; black 
+`.center` | Apply to a block wrapper to horizontally and vertically center a group of elements
+`.text` | allows the text to scroll vertically. *Apply the entire slide, not just the content*
+`.caption` | Apply to a block wrapper to align a section to the bottom of the screen
+
+#### Caption Styles
+Classes for a block element `.caption` (a child of the `.content` element)
+Class | Description
+-------------------
+*default* | aligned to bottom of frame, centered, black. All text styles can be applied specifically to a caption.
+`.gradient` | applies a black-to-transparent background gradient to the caption
+`.whiteGradient` | same as above, but with a white gradient
+`.block` | applies a solid white background the caption
--- powerppt.js
+++ powerppt.js
@@ -1,58 +1,165 @@
 (function($){ 
-   $.fn.powerPPT = function(slide) {
-       if(!slide){ slide='.slide'; }
-           
-       var transitionEnd = 'transitionend webkitTransitionEnd oTransitionEnd otransitionend animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd',
-           index,
-           cur = $(this).children(slide).eq(0),
-           slideClass = slide.substr(1);
-       
-           
-       $(cur).addClass('visible');
-       
-        keypress.combo('right', function() {
-            index = $(cur).siblings(slide).addBack().index(cur);
-            if(index < $(cur).siblings(slide).length){
-                target = index+1; 
-                cur = $(cur).siblings(slide).addBack().eq(target);    		
-                $(cur).addClass('slideLeft recent').one(transitionEnd, function(){ 
-                    $(this).addClass('visible').removeClass('slideLeft recent').prevAll().removeClass('visible slideLeft slideRight recent');
-                }); 
-                $('.recent').not(cur).removeClass('recent');
+    $.fn.extend({
+        powerPPT: function(options) {
+            var defaults = {
+                bgimgs: true,
+                controls: false,
+                loader: false,
+                origin: false,
+                slide: '.slide'
             }
-        });
-        keypress.combo('left', function() {
-            index = $(cur).siblings(slide).addBack().index(cur);
-            if(index > 0){
-                target = index-1;
-                cur = $(cur).siblings(slide).addBack().eq(target);
+            
+            var options = $.extend(defaults, options);
+            
+            return this.each(function() {
+                var o = options,
+                    transitionEnd = 'transitionend webkitTransitionEnd oTransitionEnd otransitionend animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd',
+                    index,
+                    cur,
+                    save = null,
+                    slide = o.slide,
+                    slides = $(this).find(slide),
+                    slideClass = slide.substr(1);
+                if(o.loader != false){
+                    var loader = '<div class="loader"><img src="'+o.loader+'" /><br/>img loading</div>';
+                } else {
+                    var loader;
+                }
+                    
+                if(o.origin == false){ cur = $(this).find(slide).eq(0); }
+                else { cur = $(this).find(o.origin); }
+                $(cur).addClass('visible');
                 
-                $(cur).addClass('slideRight recent').one(transitionEnd, function() {
-                   $(this).addClass('visible').removeClass('slideRight recent').nextAll().removeClass('visible slideRight recent');
-                   //hypothetical: if i go right one, then left quickly...
-                   //so right is still moving when i hit left...
-                   //at 1, hit right now at 2, hit left back at 1... 2 is a next()
+                if(o.controls==true){
+                    var controlsMarkup = '<div id="controls"><div class="up">↑</div><div class="left">←</div><div class="right">→</div><div class="down">↓</div></div>';
+                    $(this).prepend(controlsMarkup);
+                    var controls = $('#controls'),
+                        conUp = $(controls).children('.up'),
+                        conRight = $(controls).children('.right'),
+                        conDown = $(controls).children('.down'),
+                        conLeft = $(controls).children('.left'),
+                        manageControls = function(slide) {
+                            if(slide.parent().hasClass(slideClass) == false){ $(conUp).css('visibility','hidden'); }
+                            else if($(conUp).css('visibility') == 'hidden') { $(conUp).css('visibility','visible'); }
+                            
+                            if((slide.next().hasClass(slideClass) == false) && (slide.hasClass('jump') == false)){ $(conRight).css('visibility','hidden'); }
+                            else if($(conRight).css('visibility') == 'hidden') { $(conRight).css('visibility','visible'); }
+                            
+                            if(slide.prev().hasClass(slideClass) == false){ $(conLeft).css('visibility','hidden'); }
+                            else if($(conLeft).css('visibility') == 'hidden') { $(conLeft).css('visibility','visible'); }
+                            
+                            if(slide.children('.'+slideClass).length <= 0){ $(conDown).css('visibility','hidden'); }
+                            else if($(conDown).css('visibility') == 'hidden') { $(conDown).css('visibility','visible'); }
+                        };
+                } else {
+                    var manageControls = function(){ return false; };
+                }
+                if(o.bgimgs == true){
+                    $.each(slides, function() {
+                         var slide = $(this).not(o.bgignore).children('.content'),
+                             img = $(slide).children('img').eq(0);
+                         if(img.length > 0){
+                             var url = $(img).attr('src');
+                             $(img).hide();
+                             $(slide).prepend(loader);
+                             $(img).one('load', function(){
+                                 $(slide).css("background-image", "url('"+url+"')").children('.loader').remove();
+                                 $(img).remove();
+                             });
+                         }
+                    });
+                }
+                
+                $(document).keydown(function(event) {
+                    switch (event.keyCode) {
+                        case 27: //escape key
+                            if(cur.attr('data-target')){ //if this slide has a target for an escape
+                                save = cur, //save the current slide for reference
+                                target = save.attr('data-target'); //set the target
+                                cur = $(target); //make the target the current slide
+                                cur.addClass('jumpIn recent').one(transitionEnd, function() {
+                                	$(save).removeClass('visible');
+                                    $(this).addClass('visible').removeClass('jumpIn recent');
+                                });
+                            } else if(save != null){ //if slide doesn't have an escape, but does have a save state (I.E. it was the target of an escape)
+                                target = save;
+                                save = null;
+                                target.addClass('visible');
+                                cur.addClass('jumpOut').one(transitionEnd, function() {
+                                    $(this).removeClass('visible jumpOut recent');
+                                });
+                                cur = target;
+                            }
+                            break;
+                        case 33: //pageUp, experimental
+                        	if(cur.parent().hasClass(slideClass)){
+                        		var parents = cur.parents('.'+slideClass), //all parent slides
+                        			index = cur, //save the current slide
+                        			target = parents[parents.length-1]; //determine root parent as last slide in array
+	                        	$(target).addClass('visible'); //
+	                        	index.addClass('slideOut recent').one(transitionEnd, function() {
+                        		    $(this).removeClass('visible slideOut recent');
+                        		    $(target).removeClass('slideOut recent');
+                        		});
+                        		cur = $(target);
+	                        }
+                        	break;
+                        case 37: //left arrow
+                            index = $(cur).siblings(slide).addBack().index(cur);
+                            if(index > 0){
+                                target = index-1;
+                                cur = $(cur).siblings(slide).addBack().eq(target);
+                                
+                                $(cur).addClass('slideRight recent').one(transitionEnd, function() {
+                                   $(this).addClass('visible').removeClass('slideRight recent').nextAll().removeClass('visible slideRight recent');
+                                });
+                                $('.recent').not(cur).removeClass('recent'); //Only most recent advance should be called "Recent"
+                            }
+                            break;
+                        case 38: //up arrow
+                            if($(cur).parent().hasClass(slideClass)){ // only ascend if the target is a slide, not the container
+                                index = cur;
+                                cur = cur.parent();
+                                index.siblings('.content').parent().addBack().addClass('visible');
+                                index.addClass('slideOut').one(transitionEnd, function() {
+                                    $(this).removeClass('visible slideOut').siblings().removeClass('slideOut recent visible');
+                                });
+                            }
+                            break;
+                        case 39: //right arrow
+                            index = $(cur).siblings(slide).addBack().index(cur);
+                            if(index < $(cur).siblings(slide).length){
+                                target = index+1; 
+                                cur = $(cur).siblings(slide).addBack().eq(target);    		
+                                $(cur).addClass('slideLeft recent').one(transitionEnd, function(){ 
+                                    $(this).addClass('visible').removeClass('slideLeft recent').prevAll().removeClass('visible slideLeft slideRight recent');
+                                }); 
+                                $('.recent').not(cur).removeClass('recent');
+                            } else if($(cur).hasClass('jump')) {
+                                target = cur.attr('data-target');
+                                cur = $(target);
+                                $(cur).addClass('slideLeft recent').one(transitionEnd, function(){ 
+                                    $('.visible').not(cur).removeClass('visible slideLeft slideRight recent');
+                                    $(this).addClass('visible').removeClass('slideLeft recent');
+                                }); 
+                                $('.recent').not(cur).removeClass('recent');
+                            }
+                            break;
+                        case 40: //down arrow
+                            if($(cur).children(slide).length > 0){
+                                cur = $(cur).children(slide).eq(0);
+                                $(cur).addClass('slideUp').one(transitionEnd, function() {
+                                    $(this).addClass('visible').removeClass('slideUp').parent('.visible').removeClass('visible');
+                                });
+                            }
+                            break;
+                    }
+                    manageControls(cur);
                 });
-                $('.recent').not(cur).removeClass('recent'); //Only most recent advance should be called "Recent"
-            }
-        });
-        keypress.combo('down', function() {
-            if($(cur).children(slide).length > 0){
-                cur = $(cur).children(slide).eq(0);
-                $(cur).addClass('slideUp').one(transitionEnd, function() {
-                    $(this).addClass('visible').removeClass('slideUp').parent('.visible').removeClass('visible');
-                });
-            }
-        });
-        keypress.combo('up', function() {
-            if($(cur).parent().hasClass(slideClass)){ // only ascend if the target is a slide, not the container
-                $(cur).siblings('.content').addClass('slideDown').one(transitionEnd, function(){
-                    $(cur).parent().addClass('visible');
-                    $(cur).removeClass('visible');
-                    $(cur).siblings('.content').removeClass('slideDown');  
-                    cur = $(cur).parent(); //Reset after animation end so you don't mess up the selectors
-                });
-            }
-        });
-   } 
+                 
+                 manageControls($(cur));
+                 
+            });
+        }
+    });
 })(jQuery);
